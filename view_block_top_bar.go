7afa8a23cb2bd986d15c5a8c9520c09daa4f9386
Victor Bersy <victor.bersy@hey.com>
2022-06-25 21:59:22 +0200

    Improve readability

--- view_block_top_bar.go
+++ view_block_top_bar.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func DisplayTopBar(m model) string {
-	text := fmt.Sprintln(welcome_text) + fmt.Sprintln(goal_text) + fmt.Sprintln(instructions_text)
+	text := fmt.Sprintln(welcome_text) +
+		fmt.Sprintln(goal_text) +
+		fmt.Sprintln(instructions_text)
+
 	return block_top_bar_style.
 		Width(calculateColumnWidth(m.width, block_top_bar_ratio)).
 		Height(5).
